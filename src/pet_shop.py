f18fd7a38a78d4b57c65e23e6c4da4f9066bd441
Ross Condie <rosscondie@gmail.com>
2023-01-27 21:43:32 +0100

    added add_or_remove_cash function

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -3,3 +3,9 @@ def get_pet_shop_name(petshop):
 
 def get_total_cash(total):
     return total["admin"]["total_cash"]
+
+def add_or_remove_cash(total, num):
+    total["admin"]["total_cash"] += num
+    
+
+    
