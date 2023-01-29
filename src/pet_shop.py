4e0adc4411d4199835680d215f38cf7dbeb91ba5
Ross Condie <rosscondie@gmail.com>
2023-01-29 14:52:05 +0100

    added get_customer_count

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -49,3 +49,6 @@ def get_customer_cash(customer):
 
 def remove_customer_cash(customer, customer_cash):
     customer["cash"] -= customer_cash
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
